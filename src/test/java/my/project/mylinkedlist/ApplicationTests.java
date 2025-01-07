ffab06e1e4b36326e18933bedefb986e8461c298
the-eris-net <the.eris.net@gmail.com>
2025-01-08 02:48:29 +0900

    feat:init

---
+++ src/test/java/my/project/mylinkedlist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.mylinkedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
